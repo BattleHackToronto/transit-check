6a5ed5894356baac462552d967eef6a7c0a01bb9
Aditya Sridhar <aditya123ss@gmail.com>
2014-05-31 16:05:15 -0400

    made more changes to routes.js

--- app/models/user.js
+++ app/models/user.js
@@ -5,12 +5,11 @@ var bcrypt   = require('bcrypt-nodejs');
 // define the schema for our user model
 var userSchema = mongoose.Schema({
     id           : String,
-    phone        : String,
+    phone        : Number,  //phone1-phone2-phone3
     email        : String,
     password     : String,
     name         : String
-    }
-
+    
 });
 
 // methods ======================
