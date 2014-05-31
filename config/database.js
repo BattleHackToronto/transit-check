8b4795b4d63fa7539d91266add32c6996552bc23
Ayush Bhagat <ayushbhagat@gmail.com>
2014-05-31 15:45:02 -0400

    changed package.json to add passport local

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 /*module.exports = {
 
-	//'url' : 'mongodb://admin:P1S6QzxPETXa@$ayush:$27017/battlehacktoronto' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	//'url' : 'mongodb://admin:P1S6QzxPETXa@$ayushbhagat:$27017/battlehacktoronto' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 	'url' : 'mongodb://localhost:27017/battlehacktoronto'
 };*/
 
