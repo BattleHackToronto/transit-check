915763ca48da11603b9a83727ac24ad44d449305
Aditya Sridhar <aditya123ss@gmail.com>
2014-06-01 12:55:31 -0400

    twilio messaging done

--- app/routes.js
+++ app/routes.js
@@ -27,6 +27,38 @@ module.exports = function(app, passport) {
 		failureFlash : true // allow flash messages
 	}));
 
+	app.post('/notifySubmit', isLoggedIn, function(req, res){
+		var data = "Hey, you have a transit alert from "+req.user.fullname+" for the "+req.body.route+" - "+req.body.direction+" route: "+req.body.transitalert;
+		res.redirect('/notifySubmit/'+data);
+	});
+
+	app.get('/notifySubmit/:data', isLoggedIn, function(req, res){
+		//require the Twilio module and create a REST client
+		require("../node_modules/twilio/lib");
+var client = require('twilio')('ACfd84484ff7e2ee28734b5f3fb0629d8e', 'ebb9fb707b52dbd197544c8023f2b68c');
+
+//Send an SMS text message
+client.sendMessage({
+
+    to:'+15197812145', // Any number Twilio can deliver to
+    from: '+12264002188', // A number you bought from Twilio and can use for outbound communication
+    body: req.params.data // body of the SMS message
+
+}, function(err, responseData) { //this function is executed when a response is received from Twilio
+
+    if (!err) { // "err" is an error received during the request, if any
+
+        // "responseData" is a JavaScript object containing data received from Twilio.
+        // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+        // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+
+        console.log(responseData.from); // outputs "+14506667788"
+        console.log(responseData.body); // outputs "word to your mother."
+
+    }
+});
+	});
+
 	app.get('/profileRedirect', isLoggedIn, function(req, res) {
 		res.redirect('/profile/' + req.user._id);
 	});
