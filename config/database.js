33933a17ac97131f42106d5307620141da9000da
Ayush Bhagat <ayushbhagat@gmail.com>
2014-06-01 15:16:38 -0400

    first commit

--- config/database.js
+++ config/database.js
@@ -1,4 +1,19 @@
-module.exports = {
-	//'url' : 'mongodb://admin:P1S6QzxPETXa@ayushbhagat:27017/battlehacktoronto' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-	'url' : 'mongodb://localhost:27017/battlehacktoronto'
-};
+
+var ip_addr = process.env.OPENSHIFT_NODEJS_IP   || '127.0.0.1';
+var port    = process.env.OPENSHIFT_NODEJS_PORT || '8080';
+// default to a 'localhost' configuration:
+var url = '127.0.0.1:27017/YOUR_APP_NAME';
+// if OPENSHIFT env variables are present, use the available connection info:
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  url = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
+
+module.exports = { 'url' : url };
+
+
+//'url' : 'mongodb://admin:P1S6QzxPETXa@ayushbhagat:27017/battlehacktoronto' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+//'url' : 'mongodb://localhost:27017/battlehacktoronto'
