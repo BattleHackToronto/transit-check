3cff09ab7648f63e5aad37f51aebaa7710e53c26
Ayush Bhagat <ayushbhagat@gmail.com>
2014-06-01 21:38:18 -0400

    openshift mongodb url added

--- config/database.js
+++ config/database.js
@@ -2,10 +2,10 @@
 var ip_addr = process.env.OPENSHIFT_NODEJS_IP   || '127.0.0.1';
 var port    = process.env.OPENSHIFT_NODEJS_PORT || '8080';
 // default to a 'localhost' configuration:
-var url = '127.0.0.1:27017/YOUR_APP_NAME';
+var url = 'mongodb://localhost:27017/battlehacktoronto';
 // if OPENSHIFT env variables are present, use the available connection info:
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-  url = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  url = "mongodb://" + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
   process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
   process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
   process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
